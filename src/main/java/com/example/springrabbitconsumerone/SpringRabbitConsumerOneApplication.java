a81a287b5630e966a95665250db0c9b090cbf7c9
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-03-06 20:41:08 +0100

    c1

---
+++ src/main/java/com/example/springrabbitconsumerone/SpringRabbitConsumerOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrabbitconsumerone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitConsumerOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitConsumerOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrabbitconsumerone/SpringRabbitConsumerOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrabbitconsumerone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitConsumerOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
